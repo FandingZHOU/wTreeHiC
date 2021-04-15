1de036c53a04544861faebb2c9e01391e6e96162
FandingZHOU <73884270+FandingZHOU@users.noreply.github.com>
2021-04-16 01:12:56 +0800

    Add files via upload

--- code/diffHic/contactmatrix.R
+++ code/diffHic/contactmatrix.R
@@ -9,5 +9,30 @@ contactMatrix<-function(data_j,bin_size,ch_length){
     counts[data_j_index[i,1],data_j_index[i,2]]<-data_j_index[i,3]
     counts[data_j_index[i,2],data_j_index[i,1]]<-data_j_index[i,3]
   }
-  return(counts)
+  return(data.frame(counts))
+}
+multi_ContactMatrices<-function(input_path,out_path,bin_size,ch_length=NULL,n.cores=4){
+  sample_list <- list()
+  nbin=c()
+  for(j in 1:length(inputfile)){
+    sample_list[[j]]<-read_tsv(input_path[j])
+    nbin[j]=(max(sample_list[[j]][,4])+bin_size/2)/bin_size
+  }
+  if(is.null(ch_length)){
+    ch_length<-max(nbin)*bin_size
+  }
+  cm<-mclapply(sample_list,contactMatrix,bin_size,ch_length,mc.cores=n.cores)
+  if(!dir.exists(paste0(out_path,"/contactmatrix/"))){
+    dir.create(paste0(out_path,"/contactmatrix/"))
+  }
+  nrep<-length(inputfile)/2
+  for(j in 1:(nrep*2)){
+    if(j<=nrep){
+      write.table(cm[[j]], file=paste0(out_path,"/contactmatrix/G1_cm",j,".tsv"), sep="\t",quote=FALSE, row.names=FALSE)
+    }else{
+      write.table(cm[[j]], file=paste0(out_path,"/contactmatrix/G2_cm",j-nrep,".tsv"), sep="\t",quote=FALSE, row.names=FALSE)
+    }
+  }
+  return(cm)
+
 }
