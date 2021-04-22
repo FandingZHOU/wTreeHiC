b0285a688fc22dc3582105e790f81fee97428b7e
FandingZHOU <73884270+FandingZHOU@users.noreply.github.com>
2021-04-22 22:49:56 +0800

    Add files via upload

--- code/diffHic/contactmatrix.R
+++ code/diffHic/contactmatrix.R
@@ -11,7 +11,7 @@ contactMatrix<-function(data_j,bin_size,ch_length){
   }
   return(data.frame(counts))
 }
-multi_ContactMatrices<-function(input_path,out_path,bin_size,ch_length=NULL,n.cores=4){
+multi_ContactMatrices<-function(input_path,out_path,bin_size,ch_length=NULL,n.cores=4,ngroup=2){
   sample_list <- list()
   nbin=c()
   for(j in 1:length(inputfile)){
@@ -26,13 +26,49 @@ multi_ContactMatrices<-function(input_path,out_path,bin_size,ch_length=NULL,n.co
     dir.create(paste0(out_path,"/contactmatrix/"))
   }
   nrep<-length(inputfile)/2
-  for(j in 1:(nrep*2)){
-    if(j<=nrep){
-      write.table(cm[[j]], file=paste0(out_path,"/contactmatrix/G1_cm",j,".tsv"), sep="\t",quote=FALSE, row.names=FALSE)
-    }else{
-      write.table(cm[[j]], file=paste0(out_path,"/contactmatrix/G2_cm",j-nrep,".tsv"), sep="\t",quote=FALSE, row.names=FALSE)
+  if(ngroup==2){
+    for(j in 1:(nrep*2)){
+      if(j<=nrep){
+        write.table(cm[[j]], file=paste0(out_path,"/contactmatrix/G1_cm",j,".tsv"), sep="\t",quote=FALSE, row.names=FALSE)
+      }else{
+        write.table(cm[[j]], file=paste0(out_path,"/contactmatrix/G2_cm",j-nrep,".tsv"), sep="\t",quote=FALSE, row.names=FALSE)
+      }
+    }
+    
+  }
+  ##just for simulated data if we do not want to recalculate cms for original GM data 
+  if(ngroup==1){
+    for(j in 1:(nrep*2)){
+      write.table(cm[[j]], file=paste0(out_path,"/contactmatrix/G2_cm",j,".tsv"), sep="\t",quote=FALSE, row.names=FALSE)
     }
   }
-  return(cm)
-
+   return(cm)
 }
+
+merge_ContactMatrices<-function(cm,out_path,ngroup){
+  cm1=cm[[1]]
+  ncm=length(cm)
+  if(ngroup==1){
+    if(ncm>1){
+      for(j in 2:ncm){
+        cm1 <- cm1+cm[[j]]
+      }
+    }
+    write.table(cm1, file=paste0(out_path,"/aggregated_contactmatrix.tsv"), sep="\t",quote=FALSE, row.names=FALSE,col.names=F)
+  }
+  if(ngroup==2){
+    cm2=cm[[ncm/2+1]]
+    if(ncm>=4){
+      for(j in 2:(ncm/2)){
+        cm1 <- cm1+cm[[j]]
+      }
+      for(j in (ncm/2+1):ncm){
+        cm2 <- cm2+cm[[j]]
+      }
+      write.table(cm1, file=paste0(out_path,"/G1_aggregated_contactmatrix.tsv"), sep="\t",quote=FALSE, row.names=FALSE,col.names=F)
+      write.table(cm2, file=paste0(out_path,"/G2_aggregated_contactmatrix.tsv"), sep="\t",quote=FALSE, row.names=FALSE,col.names=F)
+    }
+  }
+  return(out_path)
+}
+
